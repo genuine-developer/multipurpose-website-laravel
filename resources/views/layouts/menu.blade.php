df7818eefe00374bcf7ad138ebc44cfbf703470a
intrepid.arctic.scavenger <intrepid.arctic.scavenger@gmail.com>
2021-01-03 08:03:36 +0600

    sidebar menu done

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -7,18 +7,21 @@
                         <span>Main</span>
                     </li>
                     <li class="active">
-                        <a href="index.html"><i class="fe fe-home"></i> <span>Dashboard</span></a>
+                        <a href="{{route('home')}}"><i class="fe fe-home"></i> <span>Dashboard</span></a>
+                    </li>
+                    <li class="submenu">
+                        <a href="#"><i class="fe fe-document"></i> <span> Post</span> <span class="menu-arrow"></span></a>
+                        <ul style="display: none;">
+                            <li><a href="invoice-report.html">All Post</a></li>
+                            <li><a href="invoice-report.html">Category</a></li>
+                            <li><a href="invoice-report.html">tag</a></li>
+                        </ul>
                     </li>
                   
                     <li>
                         <a href="settings.html"><i class="fe fe-vector"></i> <span>Settings</span></a>
                     </li>
-                    <li class="submenu">
-                        <a href="#"><i class="fe fe-document"></i> <span> Reports</span> <span class="menu-arrow"></span></a>
-                        <ul style="display: none;">
-                            <li><a href="invoice-report.html">Invoice Reports</a></li>
-                        </ul>
-                    </li>
+                   
                     
                       </ul>
                     </li>
