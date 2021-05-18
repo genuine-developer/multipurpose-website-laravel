9be8a3f7301f2febae81447315fd2b1bdabebe62
intrepid.arctic.scavenger <intrepid.arctic.scavenger@gmail.com>
2021-05-18 14:35:20 +0600

    vendor uploaded

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -18,6 +18,7 @@ class RouteServiceProvider extends ServiceProvider
      * @var string
      */
     public const DASH = '/dashboard';
+    public const HOME = '/home';
 
     /**
      * The controller namespace for the application.
